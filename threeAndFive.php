2ae57c263ca8df4cd6435398bf6205f2c6f481fb
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2018-12-31 12:20:20 -0500

    Checking in pseudo code

--- threeAndFive.php
+++ threeAndFive.php
@@ -1,2 +1,15 @@
 <?php
-echo "it works";
+/*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below the provided parameter value number.
+
+*/
+function threeFiveFinder($input){
+    //split into array
+    //cycle through array
+    //if the number %3 || %5 === 0, then
+    //add it to new array
+
+    //then go through array and add all those numbers together (sum)
+}
