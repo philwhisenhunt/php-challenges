534b530f570748d3261ca2b969713d1c92eff686
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2018-12-31 12:35:31 -0500

    Making progress

--- threeAndFive.php
+++ threeAndFive.php
@@ -6,10 +6,23 @@ Find the sum of all the multiples of 3 or 5 below the provided parameter value n
 
 */
 function threeFiveFinder($input){
-    //split into array
-    //cycle through array
-    //if the number %3 || %5 === 0, then
+    //make a for loop
+    $arrayOfMultiples = [];
+    for($i=0; $i < $input; $i++){
+        //if the number %3 || %5 === 0, then
+        if($i%3 === 0 || $i%5 === 0){
+            $arrayOfMultiples[] = $i;
+        }
+
+    }
+    //now add up everything in the array
+    
+    return $arrayOfMultiples;
+
+
     //add it to new array
 
     //then go through array and add all those numbers together (sum)
 }
+$input = 10;
+print_r(threeFiveFinder($input));
