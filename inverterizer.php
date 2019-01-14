827fa4d2dd72be7ecf4ca67c594f0c4c928b3b29
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-01-14 12:24:26 -0500

    checking in first attempt before test to compare later

--- inverterizer.php
+++ inverterizer.php
@@ -1,2 +1,16 @@
 <?php
 // take in a key value pair and invert it. Key becomes value and value.. becomes key.
+
+function invertirizer($input){
+    //make a new array
+    $outputArray = [];
+    //for each thing in the array as a key value pair, take the value and make it the new array, and make the array the value. 
+    foreach($input as $key=>$value){
+        $outputArray[$value] = $key;
+    }
+    return $outputArray;
+}
+
+$testArray = ['first'=>'second', 'third'=>'fourth'];
+
+echo invertirizer($testArray);
