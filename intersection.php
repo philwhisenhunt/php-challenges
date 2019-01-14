cd12ea4028fec388e15641cc3c23ca71fdecf9c2
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-01-14 13:47:36 -0500

    Fixed the test case

--- intersection.php
+++ intersection.php
@@ -7,12 +7,22 @@ function intersection($input){
     //Take a value and look for it in the next array. If its there keep going. If its there for all of them then assign that value to array. 
     //Perhaps make a function to check it?
     $intersectionArray = [];
+
+
+    // echo "The input is ";
+    // print_r($input);
+
     $firstArray = $input[0];
+
+    // echo "The first array is ";
+    // print_r($firstArray);
+    // die();
+
     for($i=1; $i<count($firstArray); $i++){
         
         for($j=0; $j<count($input); $j++){
             //if $input[$i] DOES NOT CONTAIN $firstArray[$j].. 
-            if($firstArray[$i] !== $input[$i[$j]]){
+            if($firstArray[$i] !== $input[$i][$j]){
                 return "Skip this one";
             }
                 //then return "I'm out"
@@ -25,4 +35,4 @@ function intersection($input){
     return $intersectionArray;
 }
 
-print_r(intersection([1, 2, 3], [101, 2, 1, 10], [2, 1]));
+print_r(intersection([[1, 2, 3], [101, 2, 1, 10], [2, 1]]));
