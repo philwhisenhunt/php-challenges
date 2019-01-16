07bad41ebeed5c83455de95c476fa3cd1e9388c3
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-01-16 13:14:25 -0500

    Getting wrong kind of output, but checking in current placement

--- gameBoard.php
+++ gameBoard.php
@@ -2,31 +2,9 @@
 
 //Make a gameboard
 
-//Should look like this:
-/*
---- --- --- 
-|   |   |   | 
- --- --- ---  
-|   |   |   | 
- --- --- ---  
-|   |   |   | 
- --- --- --- 
+//Tic tac toe game
 
- */
-
- $test = "  --- --- --- 
- |   |   |   | 
-  --- --- ---  
- |   |   |   | 
-  --- --- ---  
- |   |   |   | 
-  --- --- --- ";
-
-//   echo $test;
-
-  //so the first part will be to get that top row. 
-
-  function rowMaker($userNumber){
+function rowMaker($userNumber){
 
     // $userNumber = 3;
     $userMove = 0;
@@ -61,10 +39,6 @@ function colMaker($userNumber){
     echo "\n";
 }
 
-// $blankGameboard =  colMaker(3);
-// echo $blankGameboard;
-
-//now see if we can make it with arrays. 
 
 $blankGameBoard = 
         [
@@ -104,15 +78,6 @@ function convertToNewXAxis($userInputX){
     return $userTypedXAxis;
 }
 
-
-// userTypedXAxis
-// userTypedYAxis
-
-
-
-// print_r($blankGameBoard);
-// print_r($blankGameBoard);
-
 function printTheBoard($inputGameBoard){
     for($m=0; $m<count($inputGameBoard); $m++){
         for($p=0; $p<count($inputGameBoard[$m]); $p++){
@@ -130,30 +95,13 @@ function printTheBoard($inputGameBoard){
 $userTypedXAxis = 1;
 $userTypedYAxis = 1;
 
-echo "The new converted number is " . convertToNewYAxis($userTypedYAxis) . "\n";
-$userArray = [];
-// $userArray[$userTypedXAxis][$userTypedYAxis] = "| O ";
+function moveMakerO($userTypedYAxis, $userTypedXAxis){
+    $tempArray = [" O |"];
+    // $blankGameBoard = array_replace($blankGameBoard[$userTypedYAxis][$userTypedXAxis], $tempArray);
+    $blankGameBoard[$userTypedYAxis][$userTypedXAxis] = " O |";
+    return $blankGameBoard;
+}
 
-//So first, print a blank game board
-
-// printTheBoard($blankGameBoard);
-// echo "Want to play? \n";
-// echo "\n";
-
-// Assign the previous version of the array to a holding pattern, with the new array
+print_r(moveMakerO(1,1));
 
 
-      //Just do y first, then x..
-
-        /*
-
-        Used for figuring out the user conversions
-$blankGameBoard[$userTypedYAxis + 2][$userTypedXAxis + 2] = " 0 |"; //00
-$blankGameBoard[$userTypedYAxis + 4][$userTypedXAxis + 2] = " 0 |"; //10
-$blankGameBoard[$userTypedYAxis + 6][$userTypedXAxis + 2] = " 0 |"; //20
-$blankGameBoard[$userTypedYAxis + 6][$userTypedXAxis + 3] = " 0 |"; //21
-$blankGameBoard[$userTypedYAxis + 6][$userTypedXAxis + 4] = " 0 |"; //22
-*/
-
-//x axis is always the position plus 2
-//for the y axis, 
