262e8ba72b111f7b595d6490daa8e69e372c7ecc
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-01-16 12:58:58 -0500

    Added converter functions for user input

--- gameBoard.php
+++ gameBoard.php
@@ -78,9 +78,35 @@ $blankGameBoard =
         ["  ---", "  ---", "  ---"]
         ];
 
-$blankGameBoard[$userTypedXAxis + 2][$userTypedYAxis + 2] = " 0 |"; //00
-$blankGameBoard[$userTypedXAxis + 4][$userTypedYAxis + 2] = " 0 |"; //01
-$blankGameBoard[$userTypedXAxis + 6][$userTypedYAxis + 2] = " 0 |"; //02
+  
+function convertToNewYAxis($userInputY){
+
+    if($userInput === 0){
+        $userTypedYAxis = 2;
+        return $userTypedYAxis;
+
+    }
+    if($userInput === 1){
+        $userTypedYAxis = 4;
+        return $userTypedYAxis;
+
+    }
+
+    if($userInput === 2){
+        $userTypedYAxis = 6;
+        return $userTypedYAxis;
+
+    }
+}
+
+function convertToNewXAxis($userInputX){
+    $userTypedXAxis  = $userInputX + 2;
+    return $userTypedXAxis;
+}
+
+
+// userTypedXAxis
+// userTypedYAxis
 
 
 
@@ -91,6 +117,7 @@ function printTheBoard($inputGameBoard){
     for($m=0; $m<count($inputGameBoard); $m++){
         for($p=0; $p<count($inputGameBoard[$m]); $p++){
          print_r($inputGameBoard[$m][$p]);
+        
         //  echo "m$m, p$p";
          
         }
@@ -102,10 +129,31 @@ function printTheBoard($inputGameBoard){
 //accept x axis then y axis
 $userTypedXAxis = 1;
 $userTypedYAxis = 1;
+
+echo "The new converted number is " . convertToNewYAxis($userTypedYAxis) . "\n";
 $userArray = [];
 // $userArray[$userTypedXAxis][$userTypedYAxis] = "| O ";
 
 //So first, print a blank game board
-printTheBoard($blankGameBoard);
-echo "Want to play? \n";
-echo "\n";
+
+// printTheBoard($blankGameBoard);
+// echo "Want to play? \n";
+// echo "\n";
+
+// Assign the previous version of the array to a holding pattern, with the new array
+
+
+      //Just do y first, then x..
+
+        /*
+
+        Used for figuring out the user conversions
+$blankGameBoard[$userTypedYAxis + 2][$userTypedXAxis + 2] = " 0 |"; //00
+$blankGameBoard[$userTypedYAxis + 4][$userTypedXAxis + 2] = " 0 |"; //10
+$blankGameBoard[$userTypedYAxis + 6][$userTypedXAxis + 2] = " 0 |"; //20
+$blankGameBoard[$userTypedYAxis + 6][$userTypedXAxis + 3] = " 0 |"; //21
+$blankGameBoard[$userTypedYAxis + 6][$userTypedXAxis + 4] = " 0 |"; //22
+*/
+
+//x axis is always the position plus 2
+//for the y axis, 
