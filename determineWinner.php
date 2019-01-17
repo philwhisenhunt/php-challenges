686dcff4863130be3a62c1ed888fc910d687cb68
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-01-17 12:09:22 -0500

    Determining winner correctly

--- determineWinner.php
+++ determineWinner.php
@@ -1,55 +1,47 @@
 <?php
 //tic tac toe game
 
-// $board = [[1, 2, 0],
-// 	      [2, 1, 0],
-//           [2, 1, 1]];
+
 
 $board = [[1, 2, 0],
 	      [2, 1, 0],
           [2, 1, 1]];
-    
-// function printer($board){
-//     for($m=0; $m<count($board); $m++){
-//         for($p=0; $p<count($board[$m]); $p++){
-//             echo $board[$m][$p];
-//         }
-//     }
 
-// }
-    
-// print_r($game);
-// printer($board);
-/*
-for each number{
-    check the numbers to the right by changing the x axis
-    check the numbers below by changing the y axis
-    check the numbers diagonally by checking the x + 1 and the y +1
-}
+$board2 =  [[1, 2, 1],
+            [2, 2, 2],
+            [1, 1, 1]];
 
+$board3 =  [[0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]];
 
+$board3 =  [[1, 0, 0],
+            [1, 0, 0],
+            [1, 0, 0]];
 
-*/
+$board4 =  [[1, 0, 2],
+            [1, 0, 2],
+            [0, 0, 2]];
 
-//check vertical
-// [0][0]
 
 function determineWinner($board){
 
     for($i=0; $i<count($board); $i++){
         for($j=0; $j<count($board[$i]); $j++){
-            echo '$i:' . $i . "  ";
+            // echo '$i:' . $i . "  ";
            
-            echo '$j:' . $j . "  ";
-            echo "\n";
+            // echo '$j:' . $j . "  ";
+            // echo "\n";
         //check diagonal top left to bottom right
         if(! empty($board[$i + 2][$j + 2])){
-            // if(1 === 1){
-                echo "trueee";
-            // if( ($board[$i][$j] === $board[$i + 1][$j+1]) && ($board[$i][$j] === $board[$i+2][$j+2])) { 
                 if( ($board[$i][$j] === $board[$i + 1][$j+1]) && ($board[$i][$j] === $board[$i+2][$j+2])) { 
                 // return "Winner";
+
+               
                 echo "Winner diagonal tlbr";
+                echo "\n";
+
+                return $board[$i][$j];
                 echo "\n";
 
             }
@@ -60,9 +52,11 @@ function determineWinner($board){
 
             if(($board[$i][$j] === $board[$i+1][$j]) && ($board[$i][$j] === $board[$i+2][$j])){
                 // return "winner";
-                echo "Winner diagonal vertical";
+                echo "Winner vertical";
                 echo "\n";
 
+                return $board[$i][$j];
+                echo "\n";
         
             }
         }
@@ -72,19 +66,27 @@ function determineWinner($board){
 
             if(($board[$i][$j] === $board[$i][$j+1]) && ($board[$i][$j] === $board[0][$j+2])){
                 // return "winner";
-                echo "Winner diagonal horizontal";
+                echo "Winner  horizontal";
                 echo "\n";
 
+                return $board[$i][$j];
+                echo "\n";
+
+
+               
         
             }
         }
         
         //check diagonal bottom left to top right
-        if(! empty($board[$i-2][$j - 2])){
+        if(! empty($board[$i-2][$j + 2])){
 
-            if(($board[$i][$j] === $board[$i-1][$j-1]) && ($board[$i][$j] === $board[$i-2][$j-2])){
+            if(($board[$i][$j] === $board[$i-1][$j+1]) && ($board[$i][$j] === $board[$i-2][$j+2])){
                 // return "Winner";
                 echo "Winner diagonal tlbr";
+                echo "\n";
+
+                return $board[$i][$j];
                 echo "\n";
 
         
@@ -92,8 +94,7 @@ function determineWinner($board){
         }
 
         else{
-            // echo "nothing works";
-            // echo "\n";
+            // echo "OOPS";
         }
     }
 
@@ -102,6 +103,24 @@ function determineWinner($board){
 }
 }
 
+function whoWon($input){
+    if($input ===  1)
+    {
+        return "The winner is Player 1";
+        echo "\n";
+    }
+
+    if($input ===  2)
+    {
+        return "The winner is Player 2";
+        echo "\n";
+    }
+
+}
 
 
-echo determineWinner($board);
+
+// echo determineWinner($board);
+echo whoWon(determineWinner($board4));
+echo "\n";
+
