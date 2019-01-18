f0eb727190549fb69b4a9822b35fd889ae6a5fe7
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-01-18 10:24:10 -0500

    Cleaner version but losing a turn after trying to take someone's spot may be bad

--- acceptUserInput.php
+++ acceptUserInput.php
@@ -23,71 +23,71 @@ do{
     }
 
     //accept the input
-    $userInput = readline("User number $user ,Type your coordinates here ");
+    $userInput = readline("\nUser number $user ,Type your coordinates here, separated by a comma \n");
 
     //check for valid input
     if(1===1){
 
         $userInput = explode(",", $userInput);
 
-       
-
         $i = $userInput[0];
         $j = $userInput[1];
-        echo "you typed: " . $i . " and ". $j . "\n";
-        
+        echo "\nYou typed: " . $i . " and ". $j . "\n \n";
         
         //assign the new coordinate to the board
 
         if($i >= 3 || $i<0 || $j >= 3 || $j<0){
             echo "You wasted a turn! \n";
+
             if($user == 1){
                 $user = 2;
             }
+
             if($user == 2){
                 $user = 1;
             }
-
-            
-            
         }
 
-        if($board[$i][$j] == 1 || $board[$i][$j] == 2){
-            echo "Spot is taken, try again \n";
+        elseif($board[$i][$j] == 1 || $board[$i][$j] == 2){
+            echo "Spot is taken, try again \n\n";
             //But needs to not allow input. 
             if($user == 1){
                 $user = 2;
             }
+
             if($user == 2){
                 $user = 1;
             }
-        };
-
-        if($user == 1){
-            if($i < 3 && $i>=0 && $j < 3 && $j>=0){
-                // echo "hello";
-                $board[$i][$j] = 1;
-                $user = 2;
-                echo "Now it is user2's turn \n";
-
-            }  
         }
 
         else{
-            if($i < 3 && $i>=0 && $j < 3 && $j>=0){
-            $board[$i][$j] = 2;
-            $user =1;
-            echo "Now it is user1's turn \n";
+            //User 1 makes a move
+            if($user == 1){
+                if($i < 3 && $i>=0 && $j < 3 && $j>=0){
+                    $board[$i][$j] = 1;
+                    $user = 2;
+                    echo "Now it is user2's turn \n \n";
 
+                }  
+            }
+
+            //User 2 makes a move
+            else{
+                if($i < 3 && $i>=0 && $j < 3 && $j>=0){
+                    $board[$i][$j] = 2;
+                    $user =1;
+                    echo "Now it is user1's turn \n \n";
+
+                }
+            }
+
+            //Next step is to 
+            //1. remove the space steal
+            //2. No allowing J to be higher than 2.
+    
         }
-    }
-
-        //Next step is to 
-        //1. remove the space steal
-        //2. Now allow J
-   
-    }
 $continue = true;
+    }
 }
 
 while($continue === true);
