1d5d451b761284a418e6e710c5c0919337940eeb
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-01-18 09:33:39 -0500

    Now responding to input outside of scope

--- acceptUserInput.php
+++ acceptUserInput.php
@@ -8,33 +8,88 @@ $board =  [[0, 0, 0],
 
 $i = 0;
 $j = 0;
-$board[$i][$j] = 1;
+$user = 1;
 
-echo "The board: \n";
-for($i=0; $i<count($board); $i++){
-    for($j=0; $j<count($board[$i]); $j++){
-        echo $board[$i][$j] . " ";
+do{
+    //print the board
+    echo "The board: \n";
+    for($i=0; $i<count($board); $i++){
+        for($j=0; $j<count($board[$i]); $j++){
+            echo $board[$i][$j] . " ";
+        }
+    
+        echo "\n";
+    
     }
 
-    echo "\n";
+    //accept the input
+    $userInput = readline("User number $user ,Type your coordinates here ");
 
+    //check for valid input
+    if(1===1){
+
+        $userInput = explode(",", $userInput);
+
+        if($board[$i][$j]);
+
+        $i = $userInput[0];
+        $j = $userInput[1];
+        echo "you typed: " . $i . " and ". $j . "\n";
+        
+        
+        //assign the new coordinate to the board
+
+        if($i >= 3 || $i<0 || $j > 3 || $j<0){
+            echo "You wasted a turn! \n";
+            if($user == 1){
+                $user = 2;
+            }
+            if($user == 2){
+                $user = 1;
+            }
+            
+        }
+        if($user == 1){
+            if($i < 3 && $i>=0 && $j < 3 && $j>=0){
+                // echo "hello";
+                $board[$i][$j] = 1;
+                $user = 2;
+                echo "Now it is user2's turn \n";
+
+            }  
+        }
+
+        else{
+            if($i < 3 && $i>=0 && $j < 3 && $j>=0){
+            $board[$i][$j] = 2;
+            $user =1;
+            echo "Now it is user1's turn \n";
+
+        }
+    }
+
+
+        // else {
+        //     echo "You lose, game over \n";
+        //     $continue = false;
+        //     // exit();
+        // }
+
+        //Next step is to 
+        //1. remove the space steal
+        //2. Now allow J
+   
+    }
+$continue = true;
 }
 
-// $userInput = "0,0";
-$userInput = readline("Type something here: ");
+while($continue === true);
 
-//check for valid input
-if(1===1){
-    
-    $userInput = explode(",", $userInput);
-    echo "you typed: ";
-    print_r($userInput);
-
-    $i = $userInput[0];
-    $j = $userInput[1];
+echo "are we here?";
 
 
-}
+
+
 
 
 //break apart the userInput
