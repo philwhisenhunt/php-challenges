0c22a313213d051d5416c9d1662deeff03ee2d90
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-01-21 13:49:51 -0500

    on to something here, now looks right

--- acceptUserInput.php
+++ acceptUserInput.php
@@ -20,14 +20,29 @@ $user = 1;
 do{
     //print the board
     echo "The board: \n";
-    echo "  0 1 2 \n";
+    echo "  0    1    2 \n";
+    $topPiece = "  ---  ---  --- \n";
+    echo $topPiece;
     for($i=0; $i<count($board); $i++){
-        echo $i . " "; //set up the coordinates for the horizontal rows
+        echo $i . " |"; //set up the coordinates for the horizontal rows
         for($j=0; $j<count($board[$i]); $j++){
-            echo $board[$i][$j] . " ";
+            if ($board[$i][$j] == 0){
+                echo "   |";
+            }
+
+            if ($board[$i][$j] == 1){
+                echo " x |";
+            }
+
+            if ($board[$i][$j] == 2){
+                echo " o |";
+            }
+            
+            // echo $board[$i][$j] . " ";
         }
     
         echo "\n";
+        echo $topPiece;
 
     
     }
