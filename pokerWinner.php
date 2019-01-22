a44e7f3b668475582f91a1a07fdde11d803e2eef
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-01-22 16:02:39 -0500

    Checking in the mess in case I need something from it

--- pokerWinner.php
+++ pokerWinner.php
@@ -6,14 +6,17 @@
 
 $input = '1 5H 5C 6S 7S KD 2C 3S 8S 8D TD Player 2';
 
+
 //split the into an array by each space
 $arrayNow = explode(" ", $input);
 
 //Lets see what we got
-print_r($arrayNow);
+// print_r($arrayNow);
 
 echo 'Player: ' . $arrayNow[0];
 echo "\n";
+echo "\n";
+
 
 //make a player 1 array
 $player1Hand = array_slice($arrayNow, 1, 5);
@@ -24,26 +27,57 @@ $player2Hand = array_slice($arrayNow, 8, 5);
 // print_r($player2Hand);
 
 //for each thing in the players hand
-for($i=0; $i<count($player1Hand); $i++){
+$pairCount = 0;
+for($i=1; $i<count($player1Hand); $i++){
 
-    $piece = str_split($player1Hand[$i]);
-    echo "The piece is: ";
-    print_r($piece);
-    echo "\n";
+    $singleCard = str_split($player1Hand[0]);
+    echo 'singleCard is: ';
+    print_r($singleCard);
 
-    $primaryCard = $piece[0];
-    echo '$primaryCard is: ' . $primaryCard;
+    $futureCard = str_split($player1Hand[$i]);
+    echo 'futureCard is: ';
+    print_r($futureCard);
 
-    $cardSuit = $piece[1];
-    echo ' and the suit is: ' . $cardSuit;
+    // die();
 
 
-    for($j=1; $j<count($piece); $j++){
-        // if $piece[$j] === 
+    if($singleCard[0] == $futureCard[0]){
+        echo "It matches!!";
+        $pairCount++;
+
+        echo "\n";
+
         echo "\n";
     }
-    // print_r($piece);
+    // echo "The singleCard is: ";
+    // print_r($singleCard);
+    // echo "\n";
+
+/*
+    for($j=0; $j<count($singleCard); $j++){
+
+            echo "\n";
+        //compare the card here:
+        if($singleCard[0] === $player1Hand[$i]){
+            $pairCount++;
+            echo '$pairCount is now: ' . $pairCount;
+        }
+
+        $primaryCard = $singleCard[$j];
+        echo '$primaryCard is: ' . $primaryCard;
+        echo "\n";
+
+        // echo '$singleCard[1] is: ' . $singleCard[1];
+        $cardSuit = $singleCard[1];
+        echo ' and the suit is: ' . $cardSuit . "\n";
+         }
+*/
+
+    
+   
+    // print_r($singleCard);
 }
+echo 'The $pairCount is: ' . $pairCount;
 
 
 
