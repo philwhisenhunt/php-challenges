a7bfe74a1e1ba143159ffcd622457248fefc3d64
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-01-23 13:50:49 -0500

    Clean version

--- straightFinder.php
+++ straightFinder.php
@@ -12,7 +12,6 @@ function straightFinder($player1Hand){
         $positionValues[] = $card[0];
         $positionSuit[] = $card[1];
     
-        //convert the face cards into numbers, then sort the whole thing and check to see if missing anything
     }
 
     //sort position values
@@ -45,49 +44,8 @@ echo "--- \n And now for the function \n --- \n";
 echo straightFinder($player1Hand);
 echo "\n";
 
-/*
+//convert the face cards into numbers, then sort the whole thing and check to see if missing anything
 
-
-$player1Hand = '2H 3C 4S 5S 6D';
-$player1Hand = $arrayNow = explode(" ", $player1Hand);
-
-$player1Hand = array_slice($player1Hand, 0, 5);
-// print_r($player1Hand);
-
-// $positionValues = [];
-// $positionSuit = [];
-// for($i=0; $i<count($player1Hand); $i++){
-//     $card = $player1Hand[$i];
-//     $positionValues[] = $card[0];
-//     $positionSuit[] = $card[1];
-
-//     //convert the face cards into numbers, then sort the whole thing and check to see if missing anything
-// }
-echo "The position values: \n";
- print_r($positionValues);
-//  print_r($positionSuit);
-
-
-
-
-echo "--- \n And now for the function \n --- \n";
-echo straightFinder($positionValues);
-echo "\n";
-
-
-
-
-
-/*
-Might be easier to break the two part card name into two parts. 
-Take all the things and sort them
-Then check that thing for gaps
-give each card a number value.
-*/
 // $cardsInOrder = [2,3,4,5,6,7,8,9,"T","J","Q","K","A"];
-/*
-compare the card to the ordered array
-if its there, search for both the before and after in the array. 
-*/
 
 
