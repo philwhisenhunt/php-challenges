a7b932cc48313613de7dc7e6a0e393c6cad56151
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-01-23 13:49:38 -0500

    Checking in messy comments version in case needed

--- straightFinder.php
+++ straightFinder.php
@@ -1,25 +1,52 @@
 <?php
 
 
-function straightFinder($positionValues){
-    for($i=1; $i<count($positionValues); $i++){
+function straightFinder($player1Hand){
 
-       //compare to the rest of the cards
-       echo '$i is: '. $i . "\n";
+    $positionValues = [];
+    $positionSuit = [];
 
-        if($positionValues[0] == ($positionValues[$i] - 1) || $positionValues[0] == ($positionValues[$i] + 1)){
-                echo "It matches \n";
-                if($positionValues[0] == ($positionValues[$i] - 2) || $positionValues[0] == ($positionValues[$i] + 2)){
-                    echo "It matches twice \n";
+    //turn into positionValues
+    for($i=0; $i<count($player1Hand); $i++){
+        $card = $player1Hand[$i];
+        $positionValues[] = $card[0];
+        $positionSuit[] = $card[1];
     
-            }
-        }
+        //convert the face cards into numbers, then sort the whole thing and check to see if missing anything
+    }
 
-        // else{
-        //     echo "Doesn't match \n";
-        // }
+    //sort position values
+    sort($positionValues);
+    $amountInOrder = 0;
+    for($i=1; $i<count($positionValues); $i++){
+        // echo $positionValues[$i];
+        if($positionValues[$i] == ($positionValues[0]+ $i)){
+            // echo "ordered \n";
+            $amountInOrder++;
+        }
+     echo $amountInOrder . "\n";
+    }
+
+    if ($amountInOrder == 4){
+        echo "It is a straight";
+        return true;
+    }
+    else{
+        return false;
     }
 }
+//Makes a usable version of the player hand
+$player1Hand = '2H 3C 4S 5S 6D';
+$arrayNow = explode(" ", $player1Hand);
+$player1Hand = array_slice($arrayNow, 0, 5);
+
+
+echo "--- \n And now for the function \n --- \n";
+echo straightFinder($player1Hand);
+echo "\n";
+
+/*
+
 
 $player1Hand = '2H 3C 4S 5S 6D';
 $player1Hand = $arrayNow = explode(" ", $player1Hand);
@@ -27,20 +54,29 @@ $player1Hand = $arrayNow = explode(" ", $player1Hand);
 $player1Hand = array_slice($player1Hand, 0, 5);
 // print_r($player1Hand);
 
-$positionValues = [];
-$positionSuit = [];
-for($i=0; $i<count($player1Hand); $i++){
-    $card = $player1Hand[$i];
-    $positionValues[] = $card[0];
-    $positionSuit[] = $card[1];
-}
+// $positionValues = [];
+// $positionSuit = [];
+// for($i=0; $i<count($player1Hand); $i++){
+//     $card = $player1Hand[$i];
+//     $positionValues[] = $card[0];
+//     $positionSuit[] = $card[1];
+
+//     //convert the face cards into numbers, then sort the whole thing and check to see if missing anything
+// }
 echo "The position values: \n";
-//  print_r($positionValues);
+ print_r($positionValues);
 //  print_r($positionSuit);
+
+
+
 
 echo "--- \n And now for the function \n --- \n";
 echo straightFinder($positionValues);
 echo "\n";
+
+
+
+
 
 /*
 Might be easier to break the two part card name into two parts. 
@@ -48,8 +84,10 @@ Take all the things and sort them
 Then check that thing for gaps
 give each card a number value.
 */
-$cardsInOrder = [2,3,4,5,6,7,8,9,"T","J","Q","K","A"];
+// $cardsInOrder = [2,3,4,5,6,7,8,9,"T","J","Q","K","A"];
 /*
 compare the card to the ordered array
 if its there, search for both the before and after in the array. 
 */
+
+
