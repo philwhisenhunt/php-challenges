3f75cd4aaa07fd43993102cf3b660fc795760b21
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-01-23 10:15:11 -0500

    Laying a plan

--- pokerWinner.php
+++ pokerWinner.php
@@ -2,9 +2,35 @@
 /*
 1 5H 5C 6S 7S KD Pair of Fives 2C 3S 8S 8D TD Pair of Eights Player 2
 
+Take a txt file and determine how many hands player 1 wins. 
+
+So rank each type of hand
+Then compare the ranks of each hand
+If the ranks are the same, then rescan for high card. 
+
+Check for pairs
+If there is a pair check for three of a kind
+    If there is a three of a kind, check for full house
+If two pair, check for four of a kind
+If pair but not three of a kind or four of a kind, then check for two pair  
+
+check for straight:
+    for each part, check for number lower than and greater than
+    or sort the numbers and check for consequtive
+
+check for flush{
+    grab the suit
+    if the suit matches the next one, keep going
+    if they all match, check for straight
+    royal flush
+    return flush
+
+}
+
+Maybe have a different function for each thing to check?
 */
 
-$input = '1 5H 5C 6S 7S KD 2C 3S 8S 8D TD Player 2';
+$input = '5H 5C 6S 7S KD 2C 3S 8S 8D TD';
 
 
 //split the into an array by each space
@@ -19,24 +45,24 @@ echo "\n";
 
 
 //make a player 1 array
-$player1Hand = array_slice($arrayNow, 1, 5);
-// print_r($player1Hand);
+$player1Hand = array_slice($arrayNow, 0, 5);
+print_r($player1Hand);
 
 //make a player 2 array
-$player2Hand = array_slice($arrayNow, 8, 5);
-// print_r($player2Hand);
+$player2Hand = array_slice($arrayNow, 5, 5);
+print_r($player2Hand);
 
 //for each thing in the players hand
 $pairCount = 0;
 for($i=1; $i<count($player1Hand); $i++){
 
     $singleCard = str_split($player1Hand[0]);
-    echo 'singleCard is: ';
-    print_r($singleCard);
+    // echo 'singleCard is: ';
+    // print_r($singleCard);
 
     $futureCard = str_split($player1Hand[$i]);
-    echo 'futureCard is: ';
-    print_r($futureCard);
+        // echo 'futureCard is: ';
+        // print_r($futureCard);
 
     // die();
 
@@ -49,6 +75,15 @@ for($i=1; $i<count($player1Hand); $i++){
 
         echo "\n";
     }
+
+    if($pairCount == 2){
+        $threeOfAKind = 1;
+    }
+
+    if($pairCount == 3){
+        $fourOfAKind = 4;
+    }
+   
    
 
 /*
@@ -61,7 +96,7 @@ for($i=1; $i<count($player1Hand); $i++){
    
     // print_r($singleCard);
 }
-echo 'The $pairCount is: ' . $pairCount;
+echo 'The $pairCount is: ' . $pairCount . "\n";
 
 
 
