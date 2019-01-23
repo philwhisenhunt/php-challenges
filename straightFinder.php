787b430702518068c24b96f142bc38f59d17cf3e
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-01-23 13:06:47 -0500

    Not working, but checking in progress

--- straightFinder.php
+++ straightFinder.php
@@ -1,24 +1,23 @@
 <?php
 
 
-function straightFinder($player1Hand){
-    for($i=1; $i<count($player1Hand); $i++){
+function straightFinder($positionValues){
+    for($i=1; $i<count($positionValues); $i++){
 
-        $firstCard = str_split($player1Hand[0]);
-        $compareCard = str_split($player1Hand[$i]);
+       //compare to the rest of the cards
+       echo '$i is: '. $i . "\n";
 
-        if($firstCard[0] == ($compareCard[0] - 1)){
+        if($positionValues[0] == ($positionValues[$i] - 1) || $positionValues[0] == ($positionValues[$i] + 1)){
                 echo "It matches \n";
-
-                if($firstCard[0] == ($compareCard[0] - 2)){
-                    echo "Another match \n";
-                }
-
+                if($positionValues[0] == ($positionValues[$i] - 2) || $positionValues[0] == ($positionValues[$i] + 2)){
+                    echo "It matches twice \n";
+    
+            }
         }
 
-        else{
-            echo "Doesn't match \n";
-        }
+        // else{
+        //     echo "Doesn't match \n";
+        // }
     }
 }
 
@@ -26,9 +25,21 @@ $player1Hand = '2H 3C 4S 5S 6D';
 $player1Hand = $arrayNow = explode(" ", $player1Hand);
 
 $player1Hand = array_slice($player1Hand, 0, 5);
-print_r($player1Hand);
+// print_r($player1Hand);
 
-echo straightFinder($player1Hand);
+$positionValues = [];
+$positionSuit = [];
+for($i=0; $i<count($player1Hand); $i++){
+    $card = $player1Hand[$i];
+    $positionValues[] = $card[0];
+    $positionSuit[] = $card[1];
+}
+echo "The position values: \n";
+//  print_r($positionValues);
+//  print_r($positionSuit);
+
+echo "--- \n And now for the function \n --- \n";
+echo straightFinder($positionValues);
 echo "\n";
 
 /*
@@ -41,3 +52,4 @@ $cardsInOrder = [2,3,4,5,6,7,8,9,"T","J","Q","K","A"];
 /*
 compare the card to the ordered array
 if its there, search for both the before and after in the array. 
+*/
