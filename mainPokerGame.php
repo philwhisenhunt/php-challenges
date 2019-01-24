e6fd14b16074526ba81f97d1b43460d03224a312
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-01-24 15:17:38 -0500

    Now comparing cards with corrected if statement

--- mainPokerGame.php
+++ mainPokerGame.php
@@ -3,6 +3,7 @@ require 'pokerWinner.php';
 
 //take the file
 $pokerHands = fopen('smallPoker.txt', 'r') or die("Unable to open file!");
+$player1WinCount = 0;
 // echo fread($pokerHands, filesize('poker.txt'));
 
 if($pokerHands) {
@@ -33,33 +34,19 @@ if($pokerHands) {
 
         echo "\n";
 
+        if($player1HandValue > $player2HandValue){
+            $player1WinCount++;
+        }
+        
+        echo '$player1WinCount is now: ' . $player1WinCount . "\n";
+
     }
   
 
 }
 
-
-
-
-//grab the first 10 cards
-
-
-//split into an array by each space
-// $arrayNow = explode(" ", $pokerHands);
-// print_r($arrayNow);
-// die();
-
-//make a player 1 array
-// $player1Hand = array_slice($arrayNow, 0, 5);
-// echo '$player1Hand is: ';
-// print_r($player1Hand);
-// echo "\n";
-// die();
-
-//make a player 2 array
-// $player2Hand = array_slice($arrayNow, 5, 5);
-// print_r($player2Hand);
-
+//now determine the high card. 
+//now take the cards that are not in the set
 
 
 
