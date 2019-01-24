00f7de26b78a7520917cb76673fffec2211e258d
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-01-24 13:17:30 -0500

    Pulling each hand correctly now

--- mainPokerGame.php
+++ mainPokerGame.php
@@ -1,14 +1,29 @@
 <?php
 
 //take the file
-$pokerHands = fopen('poker.txt', 'r') or die("Unable to open file!");
+$pokerHands = fopen('smallPoker.txt', 'r') or die("Unable to open file!");
 // echo fread($pokerHands, filesize('poker.txt'));
 
 if($pokerHands) {
     while(($line = fgets($pokerHands)) !== false){
         echo $line;
         $arrayNow = explode(" ", $line);
-        print_r($arrayNow);
+        // print_r($arrayNow);
+        //make a player 1 array
+        $player1Hand = array_slice($arrayNow, 0, 5);
+        echo '$player1Hand is: ';
+        print_r($player1Hand);
+        echo "\n";
+
+
+        $player2Hand = array_slice($arrayNow, 5, 5);
+        echo '$player2Hand is: ';
+        print_r($player2Hand);
+        echo "\n";
+
+
+
+
     }
 }
 
@@ -21,15 +36,17 @@ if($pokerHands) {
 //split into an array by each space
 // $arrayNow = explode(" ", $pokerHands);
 // print_r($arrayNow);
-die();
+// die();
 
 //make a player 1 array
-$player1Hand = array_slice($arrayNow, 0, 5);
+// $player1Hand = array_slice($arrayNow, 0, 5);
+// echo '$player1Hand is: ';
 // print_r($player1Hand);
+// echo "\n";
 // die();
 
 //make a player 2 array
-$player2Hand = array_slice($arrayNow, 5, 5);
+// $player2Hand = array_slice($arrayNow, 5, 5);
 // print_r($player2Hand);
 
 
